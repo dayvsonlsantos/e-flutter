a6f04eafda12fdd234beed2b931223493f96e22a
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-12-29 12:02:41 -0300

    feat: Alterando a bottomAppBar

--- ola_mundo/lib/widgets/navigation_bar_app.dart
+++ ola_mundo/lib/widgets/navigation_bar_app.dart
@@ -13,15 +13,14 @@ class NavigationBarApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFF9F9F9),
-        appBarTheme: const AppBarTheme(
-          color: Color(0xFF151026),
-        ),
-        bottomAppBarTheme: const BottomAppBarTheme(
-          color: Color(0xFF151026),
-        )
-      ),
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color(0xFFF9F9F9),
+          appBarTheme: const AppBarTheme(
+            color: Color(0xFFFFFFFF),
+          ),
+          bottomAppBarTheme: const BottomAppBarTheme(
+            color: Color(0xFFFFFFFF),
+          )),
       home: const NavigationExample(),
     );
   }
@@ -41,46 +40,62 @@ class _NavigationExampleState extends State<NavigationExample> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.transparent,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.storefront_rounded,
-              color: Color(0xFF0601B4),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).bottomAppBarTheme.color,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: currentPageIndex == 0
+                  ? const Icon(
+                      Icons.storefront_rounded,
+                      color: Color(0xFF0601B4),
+                    )
+                  : const Icon(
+                      Icons.storefront_rounded,
+                      color: Color(0xFF9492DE),
+                    ),
+              onPressed: () {
+                setState(() {
+                  currentPageIndex = 0;
+                });
+              },
             ),
-            icon: Icon(Icons.storefront, color: Color(0xFF9492DE)),
-            label: 'Lojas',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.favorite,
-              color: Color(0xFF0601B4),
+            IconButton(
+              icon: currentPageIndex == 1
+                  ? const Icon(
+                      Icons.favorite,
+                      color: Color(0xFF0601B4),
+                    )
+                  : const Icon(
+                      Icons.favorite,
+                      color: Color(0xFF9492DE),
+                    ),
+              onPressed: () {
+                setState(() {
+                  currentPageIndex = 1;
+                });
+              },
             ),
-            icon: Icon(
-              Icons.favorite,
-              color: Color(0xFF9492DE),
+            IconButton(
+              icon: currentPageIndex == 2
+                  ? const Icon(
+                      Icons.settings,
+                      color: Color(0xFF0601B4),
+                    )
+                  : const Icon(
+                      Icons.settings,
+                      color: Color(0xFF9492DE),
+                    ),
+              onPressed: () {
+                setState(() {
+                  currentPageIndex = 2;
+                });
+              },
             ),
-            label: 'Favoritos',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.settings,
-              color: Color(0xFF0601B4),
-            ),
-            icon: Icon(
-              Icons.settings,
-              color: Color(0xFF9492DE),
-            ),
-            label: 'Configurações',
-          ),
-        ],
+          ],
+        ),
       ),
       body: <Widget>[
         /// Home page
@@ -134,3 +149,102 @@ class _NavigationExampleState extends State<NavigationExample> {
     );
   }
 }
+
+
+
+
+
+
+
+// return Scaffold(
+//       bottomNavigationBar: NavigationBar(
+//         onDestinationSelected: (int index) {
+//           setState(() {
+//             currentPageIndex = index;
+//           });
+//         },
+//         indicatorColor: Colors.transparent,
+//         selectedIndex: currentPageIndex,
+//         destinations: const <Widget>[
+//           NavigationDestination(
+//             selectedIcon: Icon(
+//               Icons.storefront_rounded,
+//               color: Color(0xFF0601B4),
+//             ),
+//             icon: Icon(Icons.storefront, color: Color(0xFF9492DE)),
+//             label: 'Lojas',
+//           ),
+//           NavigationDestination(
+//             selectedIcon: Icon(
+//               Icons.favorite,
+//               color: Color(0xFF0601B4),
+//             ),
+//             icon: Icon(
+//               Icons.favorite,
+//               color: Color(0xFF9492DE),
+//             ),
+//             label: 'Favoritos',
+//           ),
+//           NavigationDestination(
+//             selectedIcon: Icon(
+//               Icons.settings,
+//               color: Color(0xFF0601B4),
+//             ),
+//             icon: Icon(
+//               Icons.settings,
+//               color: Color(0xFF9492DE),
+//             ),
+//             label: 'Configurações',
+//           ),
+//         ],
+//       ),
+//       body: <Widget>[
+//         /// Home page
+//         const HomePage(),
+
+//         /// Notifications page
+//         const SecondPage(),
+
+//         /// Messages page
+//         ListView.builder(
+//           reverse: true,
+//           itemCount: 2,
+//           itemBuilder: (BuildContext context, int index) {
+//             if (index == 0) {
+//               return Align(
+//                 alignment: Alignment.centerRight,
+//                 child: Container(
+//                   margin: const EdgeInsets.all(8.0),
+//                   padding: const EdgeInsets.all(8.0),
+//                   decoration: BoxDecoration(
+//                     color: theme.colorScheme.primary,
+//                     borderRadius: BorderRadius.circular(8.0),
+//                   ),
+//                   child: Text(
+//                     'Hello',
+//                     style: theme.textTheme.bodyLarge!
+//                         .copyWith(color: theme.colorScheme.onPrimary),
+//                   ),
+//                 ),
+//               );
+//             }
+//             return Align(
+//               alignment: Alignment.centerLeft,
+//               child: Container(
+//                 margin: const EdgeInsets.all(8.0),
+//                 padding: const EdgeInsets.all(8.0),
+//                 decoration: BoxDecoration(
+//                   color: theme.colorScheme.primary,
+//                   borderRadius: BorderRadius.circular(8.0),
+//                 ),
+//                 child: Text(
+//                   'Hi!',
+//                   style: theme.textTheme.bodyLarge!
+//                       .copyWith(color: theme.colorScheme.onPrimary),
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ][currentPageIndex],
+//     );
